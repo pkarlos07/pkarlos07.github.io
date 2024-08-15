9400977e90d32fb457424caa4e29f6f1f2250404
Peter Karlos <pkarlos07@gmail.com>
2024-08-15 16:25:20 -0500

    added some rps text

--- rock_paper_scissors_TOP/main.js
+++ rock_paper_scissors_TOP/main.js
@@ -36,38 +36,38 @@ let computerScore = 0;
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === "rock") {
         if (computerChoice === "rock")
-            return "You Tie!";
+            return "Computer chose Rock. You Tie!";
         else if (computerChoice === "paper") {
             computerScore++;
-            return "You Lose! Paper beats Rock.";
+            return "Computer chose Paper. You Lose! Paper beats Rock.";
         }
         else {
             humanScore++;
-            return "You Win! Rock beats Scissors.";
+            return "Computer chose Scissors. You Win! Rock beats Scissors.";
         }
     }
     else if (humanChoice === "paper") {
         if (computerChoice === "rock") {
             humanScore++;
-            return "You Win! Paper beats Rock";
+            return "Computer chose Rock. You Win! Paper beats Rock";
         }
         else if (computerChoice === "paper")
-            return "You Tie!";
+            return "Computer chose Paper. You Tie!";
         else {
             computerScore++;
-            return "You Lose! Scissors beats Paper.";
+            return "Computer chose Scissors. You Lose! Scissors beats Paper.";
         }
     }
     else {
         if (computerChoice === "rock") {
             computerScore++;
-            return "You Lose! Rock beats Scissors.";
+            return "Computer chose Rock. You Lose! Rock beats Scissors.";
         }   
         else if (computerChoice === "paper") {
             humanScore++;
-            return "You Win! Scissors beats Paper.";
+            return "Computer chose Paper. You Win! Scissors beats Paper.";
         }
         else
-            return "You Tie!";
+            return "Computer chose Scissors. You Tie!";
     }
 }
